4d1bf795cfddabe9b8654b661441af325b9ddd19
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-11-02 00:35:33 +0300

    задание 2 готово
    Исправлены названия переменных.
    Добавлен учет повторяющихся ингридиентов.
    Ингридиенты увеличиваются на количество персон.

--- files_hw.py
+++ files_hw.py
@@ -14,21 +14,23 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
         file.readline()
         cook_book[dish_name] = ingredients
 
-# print(cook_book)
+print(cook_book)
+
 
 def get_shop_list_by_dishes(dishes, person_count):
-    d = {}
-    f = {}
-    for dishe, ingr in cook_book.items():
-        if dishe in dishes:
-            for i in ingr:
+    dict_1 = {}
+    for dish, ingred in cook_book.items():
+        if dish in dishes:
+            for i in ingred:
                 a = i.get('ingredient_name')
                 i.pop('ingredient_name')
-                if a not in d.keys():
-                    d[a] = i
-                    d[a]['quantity'] * person_count
+                if a not in dict_1.keys():
+                    dict_1[a] = i
+                    i['quantity'] *= person_count
+                else:
+                    dict_1[a]['quantity'] += i['quantity']
+    print(dict_1)
+    return
 
-
-    print(d)
 
 get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
