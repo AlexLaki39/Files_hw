d71b781f2a65625144a6d68ba025a047f8af53bd
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-11-02 00:18:14 +0300

    задание 2 - промежуточный этап
    В задание1 сделал int для количества отдельного ингредиента.
    По второму заданию сделан словарь. надо подумать как учитывать повторение ингредиента и как увеличить на количество персон.

--- files_hw.py
+++ files_hw.py
@@ -5,14 +5,30 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
         num_ingredients = int(file.readline())
         ingredients = []
         for _ in range(num_ingredients):
-            ingr = file. readline().strip().split(' | ')
+            ingr = file.readline().strip().split(' | ')
             ingredient_name, quantity, measure = ingr
             ingredients.append({'ingredient_name': ingredient_name,
-                                'quantity': quantity,
+                                'quantity': int(quantity),
                                 'measure': measure
                                 })
         file.readline()
         cook_book[dish_name] = ingredients
 
-print(cook_book)
+# print(cook_book)
 
+def get_shop_list_by_dishes(dishes, person_count):
+    d = {}
+    f = {}
+    for dishe, ingr in cook_book.items():
+        if dishe in dishes:
+            for i in ingr:
+                a = i.get('ingredient_name')
+                i.pop('ingredient_name')
+                if a not in d.keys():
+                    d[a] = i
+                    d[a]['quantity'] * person_count
+
+
+    print(d)
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
